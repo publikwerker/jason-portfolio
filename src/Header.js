57e2988c0b20fec45f88d9e8f6243366e81ce20c
Publikwerker <jason.michael.hoffman@gmail.com>
2019-04-15 12:41:04 -0700

    created favicon, body component, and put image files in the project folder

--- src/Header.js
+++ src/Header.js
@@ -1,16 +1,28 @@
 import React from 'react';
-import './header.css';
 
 export default class Header extends React.Component {
 
   render() {
+
+    const topLogo = require(('./images/yaldabaothinsta.jpg'));
+    const pubPik = require(('./images/publikwerker15r.jpg'));
     return (
-      <div className="header">
-      <img id="yaldy" alt="worm with the head of a lion" src="https://static1.squarespace.com/static/590185441b10e3a0176bb2bf/t/599f4e967131a550b1edabed/1549756325530/?format=500w" />
-      <h1 className="header-title">Jason Hoffman</h1>
-      <h2 className="bi-line">web developer | fine artist</h2>
-      <img id="my-picture" alt="jason standing in a police lineup wearing a suit vest and top hat" src="https://static1.squarespace.com/static/590185441b10e3a0176bb2bf/t/59a5ec6bd55b4150fe76e060/1504046215768/lineup.jpg?format=750w" />
-      </div>
+      <header role="banner" className="header">
+        <nav role="navigation">
+          <ul className="top-nav row">
+            <li className="top-link">about</li>
+            <li className="top-link">contact</li>
+          </ul>
+        </nav>
+        <img 
+          id="yaldy" 
+          className="not-mobile"  
+          alt="worm with the head of a lion" 
+          src={topLogo} />
+        <img id="my-picture" alt="jason standing in a police lineup wearing a suit vest and top hat" src={pubPik} />
+        <h1 className="header-title">Jason Hoffman</h1>
+        <h2 className="bi-line">full-stack developer | entrepreneur | fine artist</h2>
+      </header>
     )
   }
 }
