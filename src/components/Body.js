dd1d3758806ebfd8e383a4a2951889acb9f38933
Publikwerker <jason.michael.hoffman@gmail.com>
2019-04-16 10:56:48 -0700

    built out projectList a little mare and created DOM elements for the new object keys

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,10 +4,18 @@ import {projectList} from '../projectList';
 export default class Body extends React.Component {
   render() {
     console.log(projectList);
+    let projectDisplay=[];
+    projectList.forEach((project, index) => {
+      projectDisplay.push(<div key={index}>
+          <h2 className="project-title">{project.title}</h2>
+          <p className="project-description">{project.description}</p>
+          <p className="project-stack">{project.stack}</p>
+          <a className="project-github" href={project.github}>Github</a> 
+          <a className="project-demo" href={project.demo}>Demo</a>
+        </div>) })
     return(<div>
-    <p>this is the body</p>
-    <p>This is projectList: <br /> 
-    {projectList}</p>
+    <h1>Past Projects:   </h1>
+     {projectDisplay}
     </div>
     )
   }
--- src/projectList.js
+++ src/projectList.js
@@ -1,1 +1,21 @@
-export const projectList = [{title:'Fortune.cards'}, {title:'Traffic\'s Cool'}];
+export const projectList = [
+  {
+    title:'Fortune.cards',
+    description: 'A tarot card app with a unique turn on the traditional deck. It is designed to offer more insight into patterns in our lives.',
+    stack: 'React, Node.js, Express, MongooseDB, JWT, Redux',
+    github: 'https://github.com/thinkful-ei27/jason-react-capstone-app',
+    demo: 'https://damp-hamlet-47102.herokuapp.com/'
+  }, {
+    title:'Traffic\'s Cool',
+    description: 'A learning app designed to teach individuals the meaning of common US traffic signs. Using a spaced repetition algorithm, users are able to focus on the signs they are less familiar with.',
+    stack: 'React, Node.js, Express, MongooseDB, JWT',
+    github: 'https://github.com/thinkful-ei27/jason-react-capstone-app',
+    demo: 'https://damp-hamlet-47102.herokuapp.com/'
+  }, {
+    title:'LearnVenture',
+    description: 'Create your own LearnVenture! Build an adventure with multiple outcomes. Let users choose their own path.',
+    stack: 'React, Node.js, Express, MongooseDB, d3 Cytoscape, Redux, JWT',
+    github: 'https://github.com/thinkful-ei27/jason-react-capstone-app',
+    demo: 'https://damp-hamlet-47102.herokuapp.com/'
+  }
+];
