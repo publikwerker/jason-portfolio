4c4e8c8b1e5a0f57ae9e0c6832f7bbf486516529
Darling Press <DarlingPressStudio@DarlingNew.local>
2019-04-19 11:48:56 -0700

    adding more text to contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,6 +9,7 @@ import HeroBlock from './HeroBlock.js';
 export default class Contact extends React.Component {
   render() {
     return(<div className="contact-body">
+      <p className="contact-text">	Yes, I welcome custom, collaborative, and commissioned work.</p>
         <HeroBlock />      
         <div className="contact-info">
         <a href="mailto:someone@example.com?Subject=I%20want%20to%20hire%20you!" target="_blank" rel="noopener noreferrer"><img alt="email icon" className="contact-icon" src={emailLogo}></img></a><span className="contact-text"> jason.michael.hoffman@gmail.com</span><br />
