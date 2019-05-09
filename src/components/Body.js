b22c9d76c68e08486d6c00505b5913817ab8ac80
Darling Press <DarlingPressStudio@DarlingNew.local>
2019-05-09 14:14:29 -0700

    changed project links into buttony things, made project images larger, and project-description font smaller

--- src/components/Body.js
+++ src/components/Body.js
@@ -9,7 +9,7 @@ export default class Body extends React.Component {
       projectDisplay.push(<section key={index} className="project-box"> 
           <h3 className="project-title">{project.title}</h3>
           <img className="project-image" alt="the app as viewed in widescreen and mobile format" src={project.mockUp}></img>
-          <p className="project-description body-copy">{project.description}</p>
+          <p className="project-description">{project.description}</p>
           <p className="project-stack">{project.stack}</p>
           <a className="project-github project-link" href={project.github} target="_blank" rel="noopener noreferrer">Code</a> 
           <a className="project-demo project-link" href={project.demo} target="_blank" rel="noopener noreferrer">Demo</a>
