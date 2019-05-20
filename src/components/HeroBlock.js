46f8161d71fe945b32cdce8477c5a2c081f153ca
Publikwerker <hello@publikwerks.com>
2019-05-20 11:53:52 -0700

    increased size of my-picture

--- src/components/HeroBlock.js
+++ src/components/HeroBlock.js
@@ -5,7 +5,7 @@ export default class HeroBlock extends React.Component {
     const pubPik = require(('./images/HeadShotzJasonsqbw.jpg'));
     return (
       <div className="hero-block">
-        <img id="my-picture" alt="jason standing in a police lineup wearing a suit vest and top hat" src={pubPik} />
+        <img id="my-picture" alt="jason in a black tee standing in front of a brick wall" src={pubPik} />
         <h1 className="header-title">Jason Hoffman</h1>
         <h2 className="bi-line">full-stack developer | fine artist</h2>
       </div>
