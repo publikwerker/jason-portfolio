245d08612a0a3ed49abbc4179d3240b402911294
Jason Hoffman <jason.michael.hoffman@gmail.com>
2019-08-14 14:46:16 -0700

    fixed typo on About page

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ export default class About extends React.Component {
         <h3>About Jason <i className="smaller">a.k.a.</i> Publikwerker</h3>
         <p className="body-copy">Jason is a full-stack web developer, Fine Artist, and writer who codes elegant web apps, draws still pictures, and writes moving words. He is both congenial and tactful.</p>
         <p className="body-copy">A master of divergent thinking and creative problem solving, Jason makes use of his art and design background to craft beautiful, accessible code. He is always working for the Public Good</p>
-        <p className="body-copy">Athough he was born on the high deserts of Colorado and raised upon the turbulent waters of the Pacific Ocean, Jason now lives in SE Portland, Oregon, where he wails on his baritone ukulele, walks remote forest paths, and teaches his two children Wing Chun Kung Fu -- sometimes simultaneously.</p>
+        <p className="body-copy">Although he was born on the high deserts of Colorado and raised upon the turbulent waters of the Pacific Ocean, Jason now lives in SE Portland, Oregon, where he wails on his baritone ukulele, walks remote forest paths, and teaches his two children Wing Chun Kung Fu -- sometimes simultaneously.</p>
         <p className="body-copy">	Yes, he welcomes custom, collaborative, and commissioned work.</p>
       </main>
     )
