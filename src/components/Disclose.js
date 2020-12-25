665aee94af6469b6fa5382317a81e67c1e518c7c
Jason Hoffman <hello@publikwerks.com>
2020-12-25 14:56:03 -0800

    added disclose images, fleshed out Disclose page

--- src/components/Disclose.js
+++ src/components/Disclose.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React from 'react';
+const discloseLogo = require('./images/discloseLogo.png')
 
 export default class Disclose extends React.Component {
   render() {
     return (
       <main className="main-body">
-        <h1>.disclose()</h1>
-        <p>A cheeky grey and a space-abiding astronaut discuss the nature of reality.</p>
+      <div className="disclose-topBar">
+      {/* this will eventually be handled by a reactive header element that will swap out the logos according to page  */}
+        <h1>dot disclose comics</h1>
+        <p>More than the sum of its parts*, this irreverent comic is the result of combining my loves of drawing, 'fringe' technologies, and all things space-related.</p>
+        <img src={discloseLogo} className="disclose-logo" alt="A cartoon astronaut talking to a cartoon grey alien" />
+        <p>A cheeky grey and a space-abiding programmer discuss the nature of reality.</p>
+        <p>* <i>Please Note:</i> Although not always true, in this case the result <em>is</em> greater than the sum of its parts. See below.</p>
+        <p>( <span>heart • (pen+paper)</span> ) • ( <span>heart • (black box+alien)</span> ) • ( <span>heart • (telescope+rocket)</span> ) > ( <span>heart • (pen+paper)</span> ) + ( <span>heart • (black box+alien)</span> ) + ( <span>heart • (telescope+rocket)</span> ).</p>
+        <p>( heart cubed • ( pen+paper)(black box+alien)(telescope+rocket) ) > ( 3 heart • (pen+paper+black box+alien+telescope+rocket) )</p>
+        <p><i>Proof:</i> The love is clearly exponential in the Left Hand Side and only linear on the Right Hand Side.</p>
+      </div>
       </main>
     );
   };
