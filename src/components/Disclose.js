7b6a966c4995aaf1a1c54d1283912f699bbcf8f2
Jason Hoffman <hello@publikwerks.com>
2020-12-25 15:19:18 -0800

    altered Footer to  more closely resemble the footer on publikwerks.com. Cleaned up margins and paddings on body copy

--- src/components/Disclose.js
+++ src/components/Disclose.js
@@ -8,9 +8,9 @@ export default class Disclose extends React.Component {
       <div className="disclose-topBar">
       {/* this will eventually be handled by a reactive header element that will swap out the logos according to page  */}
         <h1>dot disclose comics</h1>
-        <p>More than the sum of its parts*, this irreverent comic is the result of combining my loves of drawing, 'fringe' technologies, and all things space-related.</p>
+        <p>More than the sum of its parts*, this irreverent comic is the result of my loves for drawing, 'fringe' technologies, and all things space-related.</p>
         <img src={discloseLogo} className="disclose-logo" alt="A cartoon astronaut talking to a cartoon grey alien" />
-        <p>A cheeky grey and a space-abiding programmer discuss the nature of reality.</p>
+        <p>A cheeky grey alien and a space-abiding programmer discuss the nature of reality.</p>
         <p>* <i>Please Note:</i> Although not always true, in this case the result <em>is</em> greater than the sum of its parts. See below.</p>
         <p>( <span>heart • (pen+paper)</span> ) • ( <span>heart • (black box+alien)</span> ) • ( <span>heart • (telescope+rocket)</span> ) > ( <span>heart • (pen+paper)</span> ) + ( <span>heart • (black box+alien)</span> ) + ( <span>heart • (telescope+rocket)</span> ).</p>
         <p>( heart cubed • ( pen+paper)(black box+alien)(telescope+rocket) ) > ( 3 heart • (pen+paper+black box+alien+telescope+rocket) )</p>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,19 @@
 import React from 'react';
-
+const instaLogo = require('./images/instagram.png');
+const linkedInLogo = require('./images/linkedin.png');
 export default class Footer extends React.Component {
   render () {
     return (
       <footer className="footer">
-      <p className="footer-text">All content Copyright &copy; 2020 Publikwerks, LLC</p>
+      <div className="footer-button-box row">
+      <a className="button" href="https://www.instagram.com/publikwerker" >
+        <img className="footer-link" src={instaLogo} alt="instagram logo" ></img>
+      </a>
+      <a className="button" href="https://www.linkedin.com/in/publikwerker" >
+        <img className="footer-link" src={linkedInLogo} alt="LinkedIn logo" ></img>
+      </a>
+      </div>
+      <p className="footer-text">All content Copyright &copy; 2021 Publikwerks, LLC</p>
       </footer>
     )
   }
